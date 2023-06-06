4a20f4da6a79c6a2a22578bd81873763e4395d4d
abdulbasit-png <beapunjabian12@gmail.com>
2023-06-06 12:27:03 -0700

    Initial commit

---
+++ src/main/java/com/example/primeface/PrimefaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.primeface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimefaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimefaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/primeface/PrimefaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.primeface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimefaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
